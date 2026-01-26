d236354d936870de1349b7ce8d2d4a1382fd2a3a
prtchal <166069848+prtchal@users.noreply.github.com>
2026-01-26 11:13:18 -0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -17,7 +17,8 @@ def get_daily_term():
     
     chat_completion = client.chat.completions.create(
         messages=[{"role": "user", "content": prompt}],
-        model="llama3-8b-8192",
+       #--- model="llama3-8b-8192", ---
+        model="llama-3.3-70b-versatile",
         response_format={"type": "json_object"}
     )
     return json.loads(chat_completion.choices[0].message.content)
